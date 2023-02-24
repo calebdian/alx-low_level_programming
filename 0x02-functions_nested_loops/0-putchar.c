7e5bfb2466f9935169bfb4c149e2473480f7804b
calebdian <toromocaleb2002@gmail.com>
2023-02-24 14:43:29 -0800

    question finiheed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +15,5 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
 
